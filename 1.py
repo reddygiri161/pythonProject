5258af391b218073c07d751f084407c29fa7c9fb
reddygiri161 <100840205+reddygiri161@users.noreply.github.com>
2022-06-28 14:38:41 +0530

    Initial commit

---
+++ 1.py
@@ -0,0 +1,11 @@
+while True:
+    var = int(input("enter the college registered number: U19CS"))
+    if var>=1:
+        print("welcome to bhararth university")
+        a = input("do u wanna check your attendance yes/no:")
+        if a=='yes':
+            print("your attendance is here")
+        else:
+            print("fuck off")
+    elif var<=500:
+        print("your number is not in the list")
